eefded39864c2e838c5fddc70bb6fe456406abe5
maha-abdelzaher <maha.abdelzaher@gmail.com>
2022-01-29 15:01:49 +0200

    route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage'
+
+const Hatepage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+);
+
 function App() {
   return (
-    <div> <Homepage/> </div>
+    <div> 
+      <Switch>
+        <Route exact path="/" component={Homepage}/>
+        <Route exact path="/hats" component={Hatepage}/>
+      </Switch>
+    </div>
   );
 }
 
